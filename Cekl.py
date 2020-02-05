3f0015b548607af1772116ac32a1f3fe7e124045
User <akobcev@mail.ru>
2020-02-05 21:13:44 +0400

    lekcia 2

---
+++ Cekl.py
@@ -0,0 +1,117 @@
+#Задачи на циклы и оператор условия------
+#-Вывести на экран циклом пять строк из нулей, причем каждая строка должна быть пронумерована.
+#integer_number = 54321
+#integer_number2 = 00000
+
+
+#while integer_number>0:
+#     print(integer_number%10,integer_number2%10)
+#     integer_number = integer_number//10
+#     integer_number2 = integer_number2//10
+
+
+"""
+Задача 2
+Пользователь в цикле вводит 10 цифр. Найти количество введеных пользователем цифр 5.
+"""
+#x=int(input("Введите число"))
+#kol=0
+#while x>0:
+
+#  if x%10==5:
+#    kol+=1
+#  x=x//10
+#print('Всего пятерок:', kol)
+
+'''
+Задача 3
+Найти сумму ряда чисел от 1 до 100. Полученный результат вывести на экран.
+'''
+#sum = 0
+#for i in range(1,101):
+#   sum+=i
+#print(sum)
+
+'''
+Задача 4
+Найти произведение ряда чисел от 1 до 10. Полученный результат вывести на экран.
+'''
+#
+#pr = 1
+#for i in range(1,11):
+# pr*=i
+#print(pr)
+
+'''
+Задача 5
+Вывести цифры числа на каждой строчке.
+'''
+#Не смог сообразить как вывести прямым порядком
+# integer_number = 2129
+#
+# #print(integer_number%10,integer_number//10)
+#
+# while integer_number>0:
+#     print(integer_number%10)
+#     integer_number = integer_number//10
+
+'''
+Задача 6
+Найти сумму цифр числа.
+'''
+#integer_number = 2129
+#s=0
+#while integer_number>0:
+#   last=integer_number%10
+#   s=s+last
+#   integer_number=integer_number//10
+#print("Сумма всех цифр:",s)
+
+'''
+Задача 7
+Найти произведение цифр числа.
+'''
+#x = 2129
+#pr=1
+#while x>0:
+#    last=x%10
+#    pr=pr*last
+#    x=x//10
+#print("Произведение всех цифр:",pr)
+'''
+Задача 8
+Дать ответ на вопрос: есть ли среди цифр числа 5?
+'''
+#integer_number = 2134135
+#while integer_number>0:
+#    if integer_number%10 == 5:
+#        print('Yes')
+#        break
+#    integer_number = integer_number//10
+#else: print('No')
+
+'''
+Задача 9
+Найти максимальную цифру в числе
+'''
+#x = 2129   # Не могу понять почему не работает?
+#maxim=0
+#while x>0:
+#    last=x%10
+#    maxim<last
+#    maxim=last
+#    x=x//10
+#print("Maxim:",maxim)
+'''
+Задача 10
+Найти количество цифр 5 в числе
+'''
+#n=2129555
+#s=0
+#while n>0:
+
+#    if n%10==5:
+#      s+=1
+#    n=n//10
+#print('количество пятерок',s)
+
